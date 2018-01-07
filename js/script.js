16aaef4143c23ff806ad0aa52e71eef01a4e00fa
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-07 20:26:18 +0530

    Functions!

--- js/script.js
+++ js/script.js
@@ -2,9 +2,28 @@
  * JS
  */
 
-let inRing = 'Hulk Hogan';
-let onSide = 'The Rock';
+ function convertCurrency(amount) {
+  const converted = {
+    USD: amount * 0.76,
+    GPB: amount * 0.53,
+    AUD: amount * 1.01,
+    MEX: amount * 13.30
+  };
+  return converted;
+}
 
-console.log(inRing, onSide);
-[inRing, onSide] = [onSide, inRing];
-console.log(inRing, onSide);
+const { USD, GPB, AUD, MEX } = convertCurrency(100);
+console.log(USD, GPB, AUD, MEX);
+
+function tipCalc({
+  total = 100, 
+  tax = 0.13, 
+  tip = 0.15,
+} = {}) {
+  return total + (tip * total) + (tax * total);
+}
+
+const bill = tipCalc({});
+console.log(bill);
+
+
